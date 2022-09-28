27b9f306cf07ee3b7b01c26ab97210779a9f1b95
Mathieu Florentin <mathieu.florentin27@gmail.com>
2022-09-28 20:28:24 +0200

    Clean code and sending fake data for workout list

--- server/index.js
+++ server/index.js
@@ -24,17 +24,28 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-// All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+const FAKE_WORKOUTS_TEMP = [
+  {
+    name: "Pull ups",
+  },
+  {
+    name: "Push ups",
+  },
+  {
+    name: "Squats",
+  },
+  {
+    name: "Deadlifts",
+  },
+];
+
+app.get("/workouts", (req, res) => {
+  res.json({ data: FAKE_WORKOUTS_TEMP });
 });
 
 app.listen(PORT, () => {
