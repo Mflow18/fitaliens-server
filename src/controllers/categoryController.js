62e98af9864cbbba754ebc9b54c0ebeb96194f16
Mathieu Florentin <mathieu.florentin27@gmail.com>
2023-03-07 09:52:50 +0100

    Move methods in categoryController

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,57 @@
+const Category = require("../data/models/category");
+
+const getCategories = (req, res) => {
+  Category.find({})
+    .lean()
+    .exec((err, doc) => {
+      res.json({ data: doc });
+    });
+};
+
+const createCategory = async (req, res) => {
+  const category = await Category.findOne({ name: req.body.categories });
+
+  Category.create({
+    name: req.body.name,
+    categories: category,
+  })
+    .then(() => {
+      Category.find({})
+        .lean()
+        .exec((err, doc) => {
+          res
+            .status(201)
+            .json({ message: "Category created successfully", data: doc });
+        });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error,
+      });
+    });
+};
+
+const deleteCategory = (req, res) => {
+  Category.find({ name: req.body.name })
+    .remove()
+    .then(() => {
+      Category.find({})
+        .lean()
+        .exec((err, doc) => {
+          res
+            .status(201)
+            .json({ message: "Category created successfully", data: doc });
+        });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error,
+      });
+    });
+};
+
+module.exports = {
+  getCategories,
+  createCategory,
+  deleteCategory,
+};
--- src/routes/categories/categories.js
+++ src/routes/categories/categories.js
@@ -2,51 +2,16 @@ const Category = require("../../data/models/category");
 const express = require("express");
 const app = express.Router();
 
-app.get("/", (req, res) => {
-  Category.find({})
-    .lean()
-    .exec((err, doc) => {
-      res.json({ data: doc });
-    });
-});
+const {
+  getCategories,
+  createCategory,
+  deleteCategory,
+} = require("../../controllers/categoryController.js");
 
-app.post("/", (req, res) => {
-  Category.create({
-    name: req.body.name,
-  })
-    .then(() => {
-      Category.find({})
-        .lean()
-        .exec((err, doc) => {
-          res
-            .status(201)
-            .json({ message: "Category created successfully", data: doc });
-        });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
-});
+app.get("/", getCategories);
 
-app.delete("/", (req, res) => {
-  Category.find({ name: req.body.name })
-    .remove()
-    .then(() => {
-      Category.find({})
-        .lean()
-        .exec((err, doc) => {
-          res
-            .status(201)
-            .json({ message: "Category created successfully", data: doc });
-        });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
-});
+app.post("/", createCategory);
+
+app.delete("/", deleteCategory);
 
 module.exports = app;
