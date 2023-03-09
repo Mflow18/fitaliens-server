af334649dd4bb0a076eca88640e4ee800179f9d7
Mathieu Florentin <mathieu.florentin27@gmail.com>
2023-03-09 15:51:52 +0100

    Add workout router

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const exercisesRouter = require("./exercises/exercises");
 const categoriesRouter = require("./categories/categories");
 const musclesRouter = require("./muscles/muscles");
+const workoutsRouter = require("./workouts/workouts");
+
 const app = express();
 
 app.use("/exercises", exercisesRouter);
 app.use("/categories", categoriesRouter);
 app.use("/muscles", musclesRouter);
+app.use("/workouts", workoutsRouter);
 
-app.get("/workouts", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
